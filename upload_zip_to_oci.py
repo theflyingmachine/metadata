4d8c33add7c814b342822499d0baf60a69f31ddf
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2025-05-30 12:35:08 +0530

    update

--- upload_zip_to_oci.py
+++ upload_zip_to_oci.py
@@ -2,31 +2,20 @@ import ast
 import oci
 import os
 
-# Read private key content from file
 with open(os.environ["OCI_KEY_FILE"], "r") as f:
     WEB_CONFIG = f.read()
 
-# === USER INPUT ===
 bucket_name = 'LightsOn-Metadata-bucket'
 namespace = 'bmsfecivotax'  # Replace with your actual namespace
 file_path = os.environ["file_path"]
 object_name = os.path.basename(file_path)
-
-# === INITIATE OBJECT STORAGE CLIENT ===
-print(ast.literal_eval(WEB_CONFIG))
-print(type(ast.literal_eval(WEB_CONFIG)))
-
 object_storage = oci.object_storage.ObjectStorageClient(ast.literal_eval(WEB_CONFIG))
 
-# === UPLOAD FILE ===
-try:
-    with open(file_path, 'rb') as f:
-        put_object_response = object_storage.put_object(
-            namespace_name=namespace,
-            bucket_name=bucket_name,
-            object_name=object_name,
-            put_object_body=f
-        )
-    print(f"Upload successful: {object_name}")
-except Exception as e:
-    print(f"Error uploading file: {e}")
+with open(file_path, 'rb') as f:
+    put_object_response = object_storage.put_object(
+        namespace_name=namespace,
+        bucket_name=bucket_name,
+        object_name=object_name,
+        put_object_body=f
+    )
+print(f"Upload successful: {object_name}")
